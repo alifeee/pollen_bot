ae7fbdd7eb7e067305414833f0777c2818b6a82c
alifeee <alifeeerenn@hotmail.com>
2023-06-17 00:53:54 +0100

    change get_forecast_message signature

--- pollenbot/bothandlers/forecast.py
+++ pollenbot/bothandlers/forecast.py
@@ -2,7 +2,12 @@
 from telegram import Update
 from telegram import *
 from telegram.ext import *
-from ..forecast import Forecast, get_forecasts, format_pollen_level
+from ..forecast import (
+    Forecast,
+    get_forecasts,
+    format_pollen_level,
+    get_forecast_for_region,
+)
 import datetime
 
 _FORECAST_MESSAGE = """
@@ -14,7 +19,7 @@ The rest of the week:
 """
 
 
-def get_forecast_message(forecasts: list[Forecast], region_id: str) -> str:
+def get_forecast_message(forecast: Forecast) -> str:
     """Get the forecast message for a region.
 
     Args:
@@ -26,19 +31,9 @@ def get_forecast_message(forecasts: list[Forecast], region_id: str) -> str:
     Returns:
         str: Forecast message
     """
-    region_forecast = next(
-        (
-            reg_forecast
-            for reg_forecast in forecasts
-            if reg_forecast.region.id == region_id
-        ),
-        None,
-    )
-    if region_forecast is None:
-        raise ValueError(f"Region {region_id} not found in forecast")
 
-    region_name = region_forecast.region.name
-    days = region_forecast.days
+    region_name = forecast.region.name
+    days = forecast.days
     pollen_levels = [format_pollen_level(day.pollen_level) for day in days]
     days_names = [day.date.strftime("%A") for day in days]
 
@@ -66,7 +61,12 @@ async def _forecast(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     forecasts = get_forecasts()
-    await update.message.reply_text(get_forecast_message(forecasts, region_id))
+    region_forecast = get_forecast_for_region(forecasts, region_id)
+    if region_forecast is None:
+        await update.message.reply_text("Region not found! Try /start")
+        return
+
+    await update.message.reply_text(get_forecast_message(region_forecast))
 
 
 forecast_handler = CommandHandler("forecast", _forecast)
