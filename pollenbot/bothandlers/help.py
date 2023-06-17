d27731944ee1538c08b4052efa977fd199dd81a4
alifeee <alifeeerenn@hotmail.com>
2023-06-17 01:33:01 +0100

    add met office info

--- pollenbot/bothandlers/help.py
+++ pollenbot/bothandlers/help.py
@@ -4,6 +4,9 @@ from telegram.ext import CommandHandler
 _HELP_MESSAGE = """
 Use /start to set up, or /forecast to get the forecast for your region.
 
+This bot pulls data from the Met Office pollen forecast:
+https://www.metoffice.gov.uk/weather/warnings-and-advice/seasonal-advice/pollen-forecast
+
 Privacy:
 Naturally, I store your region and preferences. If you use /start and choose not to set up the bot, this information will be deleted.
 
